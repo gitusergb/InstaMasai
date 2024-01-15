234c575092e4709658ccf33c3294c35c44b3c0b1
gitusergb <g4ur131@gmail.com>
2024-01-15 14:46:41 +0530

    backend

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+ const connection = mongoose.connect(process.env.mongoconnect, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    console.log('Connected to MongoDB');
+
+    module.exports = {
+        connection
+    }
+
+     
+ 
+    
---
+++ index.js
@@ -0,0 +1,44 @@
+
+const cors = require('cors');
+require('dotenv').config();
+// const bodyParser = require('body-parser');
+
+const express = require('express');
+const mongoose = require('mongoose');
+ 
+const {connection} = require("./db")
+// const {userRouter}= require("./Routes/user.routes");
+// const { postRouter } = require('./Routes/post.routes');
+
+const app = express();
+
+app.use(express.json())
+// app.use(express.static('public'));
+ app.use(cors())
+
+  
+
+// app.use("/users",userRouter)
+// app.use("/posts",noteRouter)
+app.use("/",(req,res)=>{
+    res.send("post app")
+    console.log("post app")
+})
+
+  const PORT = process.env.PORT || 3000;
+
+    async function startServer() {
+        try {
+          await connection;
+          console.log("Database connected");
+        } catch {
+          console.log("Database connect Failed");
+        }
+      
+        app.listen(PORT,() => {
+          console.log(`Server is running at http://localhost:${PORT}`);
+          console.log("Server Started");
+        });
+      }
+      
+      startServer();
